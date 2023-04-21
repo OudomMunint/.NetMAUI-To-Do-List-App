3c117b143871011bc46af3f1ea8e7b621b3f5cc0
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-04-21 23:09:57 +1000

    Build fail issue

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,18 +1,28 @@
-﻿namespace ToDoListApp;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Media;
+using System;
+using System.Reflection;
+
+
+namespace ToDoListApp;  
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
