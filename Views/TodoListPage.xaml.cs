87797631959d2888a3e5347afed871030b7260ba
OudomMunint <oudommunint@gmail.com>
2023-09-03 21:27:03 +1000

    Feat: Dynamic title, delete all, date time format, !Done icons, settings page.

--- Views/TodoListPage.xaml.cs
+++ Views/TodoListPage.xaml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ToDoListApp.Data;
 using ToDoListApp.Models;
 
@@ -19,8 +14,14 @@ namespace ToDoListApp.Views
         protected override async void OnAppearing()
         {
             base.OnAppearing();
+            await UpdateListView();
+        }
+
+        private async Task UpdateListView()
+        {
             TodoitemDatabase database = await TodoitemDatabase.Instance;
             listView.ItemsSource = await database.GetItemsAysnc();
+            UpdateTitle();
         }
 
         async void OnItemAdded(object sender, EventArgs e)
@@ -31,11 +32,36 @@ namespace ToDoListApp.Views
             });
         }
 
+        async void OpenSettings(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        async void DeleteAllItems(object sender, EventArgs e)
+        {
+            bool userConfirmed = await DisplayAlert("Delete All Tasks", "Confirm you want to DELETE ALL items?", "Yes", "No");
+
+            if (userConfirmed)
+            {
+                TodoitemDatabase database = await TodoitemDatabase.Instance;
+                var allitems = await database.GetItemsAysnc();
+
+                foreach (var item in allitems)
+                {
+                    await database.DeleteItemAsync(item);
+                }
+
+                listView.ItemsSource = null;
+                await UpdateListView();
+            }
+        }
+
         async void OnDeleteClicked(object sender, EventArgs e)
         {
             var todoItem = (Todoitem)BindingContext;
             TodoitemDatabase database = await TodoitemDatabase.Instance;
             await database.DeleteItemAsync(todoItem);
+            await UpdateListView(); // Update the ListView after deleting an item.
             await Navigation.PopAsync();
         }
 
@@ -50,6 +76,10 @@ namespace ToDoListApp.Views
             }
         }
 
+        private void UpdateTitle()
+        {
+            int totalItems = listView.ItemsSource?.Cast<object>().Count() ?? 0;
+            Title = $"Home - {totalItems} Tasks Open";
+        }
     }
 }
-
