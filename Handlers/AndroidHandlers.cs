b8a82abdea04fa042a3719d3af2f0eef1aeb8112
OudomMunint <oudommunint@gmail.com>
2023-11-24 11:08:49 +0700

    Added missing usings

--- Handlers/AndroidHandlers.cs
+++ Handlers/AndroidHandlers.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Graphics;
-//using Microsoft.Maui.Controls.Shapes;
-//using Microsoft.Maui.Platform;
-//using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Shapes;
+using Microsoft.Maui.Platform;
+using Microsoft.Maui.Controls;
 
 #if ANDROID
 using Microsoft.Maui.Controls.Compatibility.Platform.Android;
