0ef1366d56ffecdc8a33c9bb16df7b6bfc38424a
OudomMunint <oudommunint@gmail.com>
2024-03-28 19:59:09 +1100

    UI - Header view

--- Views/HeaderGrid.xaml.cs
+++ Views/HeaderGrid.xaml.cs
@@ -1,9 +1,42 @@
-namespace ToDoListApp.Views;
+using Microsoft.Maui.Controls;
+using System;
 
-public partial class HeaderGrid : ContentView
+namespace ToDoListApp.Views
 {
-	public HeaderGrid()
-	{
-		InitializeComponent();
-	}
+    public partial class HeaderGrid : ContentView
+    {
+        private readonly TimeSpan updateInterval = TimeSpan.FromMinutes(1);
+        private readonly System.Threading.Timer timer;
+
+        public HeaderGrid()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            // Update every minute
+            timer = new System.Threading.Timer(UpdateCurrentDate, null, TimeSpan.Zero, updateInterval);
+            
+            // Set initial value
+            UpdateCurrentDate(null);
+        }
+
+        private string _currentDate;
+        public string CurrentDate 
+        { 
+            get => _currentDate; 
+            set
+            {
+                if (_currentDate != value)
+                {
+                    _currentDate = value;
+                    OnPropertyChanged(nameof(CurrentDate));
+                }
+            }
+        }
+
+        private void UpdateCurrentDate(object state)
+        {
+            CurrentDate = DateTime.Now.ToString("🗓️ MMMM dd, yyyy, hh:mm tt");
+        }
+    }
 }
