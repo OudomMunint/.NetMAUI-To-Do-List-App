2caab3ae8227abc96626ce3c347af71601755e4c
OudomMunint <oudommunint@gmail.com>
2024-05-12 23:41:40 +1000

    Custom view cell

---
+++ CustomViewCell.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace ToDoListApp;
+
+public class CustomViewCell : Microsoft.Maui.Controls.ViewCell
+{
+    public static readonly BindableProperty SelectedBackgroundColorProperty = BindableProperty.Create(nameof(SelectedBackgroundColor), typeof(Color), typeof(CustomViewCell), Colors.White);
+
+    public Color SelectedBackgroundColor
+    {
+        get { return (Color)GetValue(SelectedBackgroundColorProperty); }
+        set { SetValue(SelectedBackgroundColorProperty, value); }
+    }
+
+    public CustomViewCell()
+    {
+
+    }
+}
