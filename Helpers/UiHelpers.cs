cdb5c98042008e6f98f7910789afd219b06c044d
OudomMunint <oudommunint@gmail.com>
2025-01-17 22:01:50 +1100

    House keeping - Helper class

---
+++ Helpers/UiHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListApp.Helpers;
+public static class UiHelpers
+{
+    public static void SetSwitchColors(IEnumerable<Switch> switches)
+    {
+        foreach (var s in switches)
+        {
+            s.ThumbColor = Colors.White;
+
+            s.Toggled += (sender, e) =>
+            {
+                s.ThumbColor = Colors.White;
+                s.OnColor = Colors.LimeGreen;
+            };
+        }
+    }
+}
